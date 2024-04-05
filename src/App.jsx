c3be1acc96fb9a80d50c16863471328f30ef70a7
odenra <ilkerkaraoden@gmail.com>
2024-04-05 23:01:41 +0300

    footer  section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
@@ -15,6 +16,7 @@ function App() {
         <Workflow />
         <Pricing />
         <Testimonials />
+        <Footer />
       </div>
     </>
   );
