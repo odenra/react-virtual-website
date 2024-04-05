4e09529a5944c841ae74cc6801e704a8176aa303
odenra <ilkerkaraoden@gmail.com>
2024-04-05 22:58:33 +0300

    customer review  section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <Features />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
       </div>
     </>
   );
