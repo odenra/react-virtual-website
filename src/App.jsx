f91b23fd4a00ab4c083c38a357aa111da9ba1433
odenra <ilkerkaraoden@gmail.com>
2024-04-05 18:43:02 +0300

    hero section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
